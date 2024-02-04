7cafdd832fcdf0a3d4c968b78086f20b688ec673
simba77 <simba7@inbox.ru>
2024-02-04 20:06:34 +0300

    Removed optional last slashes because now it handled by router

--- config/routes.php
+++ config/routes.php
@@ -8,43 +8,43 @@ use Johncms\Users\Middlewares\AuthorizedUserMiddleware;
 use Johncms\Users\Middlewares\HasPermissionMiddleware;
 
 return function (RouteCollection $router) {
-    $router->get('/guestbook[/]', [GuestbookController::class, 'index'])->setName('guestbook.index');
-    $router->post('/guestbook[/]', [GuestbookController::class, 'index']);
+    $router->get('/guestbook/', [GuestbookController::class, 'index'])->setName('guestbook.index');
+    $router->post('/guestbook/', [GuestbookController::class, 'index']);
 
     $router->group(
         '/guestbook',
         function (RouteCollection $router) {
-            $router->get('/ga[/]', [GuestbookController::class, 'switchGuestbookType'])->setName('guestbook.switch_type');
+            $router->get('/ga/', [GuestbookController::class, 'switchGuestbookType'])->setName('guestbook.switch_type');
 
-            $router->post('/upload_file', [GuestbookController::class, 'loadFile'])
+            $router->post('/upload_file/', [GuestbookController::class, 'loadFile'])
                 ->addMiddleware(AuthorizedUserMiddleware::class)
                 ->setName('guestbook.uploadFile');
 
-            $router->get('/edit/{id:number}', [GuestbookController::class, 'edit'])
+            $router->get('/edit/{id:number}/', [GuestbookController::class, 'edit'])
                 ->addMiddleware(new HasPermissionMiddleware('guestbook_delete_posts'))
                 ->setName('guestbook.edit');
-            $router->post('/edit/{id:number}', [GuestbookController::class, 'edit'])
+            $router->post('/edit/{id:number}/', [GuestbookController::class, 'edit'])
                 ->addMiddleware(new HasPermissionMiddleware('guestbook_delete_posts'))
                 ->setName('guestbook.edit.store');
 
-            $router->get('/delpost/{id:number}', [GuestbookController::class, 'delete'])
+            $router->get('/delpost/{id:number}/', [GuestbookController::class, 'delete'])
                 ->addMiddleware(new HasPermissionMiddleware('guestbook_delete_posts'))
                 ->setName('guestbook.delete');
-            $router->post('/delpost/{id:number}', [GuestbookController::class, 'delete'])
+            $router->post('/delpost/{id:number}/', [GuestbookController::class, 'delete'])
                 ->addMiddleware(new HasPermissionMiddleware('guestbook_delete_posts'))
                 ->setName('guestbook.delete.store');
 
-            $router->get('/reply/{id:number}', [GuestbookController::class, 'reply'])
+            $router->get('/reply/{id:number}/', [GuestbookController::class, 'reply'])
                 ->addMiddleware(new HasPermissionMiddleware('guestbook_delete_posts'))
                 ->setName('guestbook.reply');
-            $router->post('/reply/{id:number}', [GuestbookController::class, 'reply'])
+            $router->post('/reply/{id:number}/', [GuestbookController::class, 'reply'])
                 ->addMiddleware(new HasPermissionMiddleware('guestbook_delete_posts'))
                 ->setName('guestbook.reply.store');
 
-            $router->get('/clean', [GuestbookController::class, 'clean'])
+            $router->get('/clean/', [GuestbookController::class, 'clean'])
                 ->addMiddleware(new HasPermissionMiddleware('guestbook_clear'))
                 ->setName('guestbook.clean');
-            $router->post('/clean', [GuestbookController::class, 'clean'])
+            $router->post('/clean/', [GuestbookController::class, 'clean'])
                 ->addMiddleware(new HasPermissionMiddleware('guestbook_clear'))
                 ->setName('guestbook.clean.store');
         }
